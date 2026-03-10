229b2ff356c857fe3d3cbc95da006dad64fdf857
toxicboi99 <shaharyan93277@gmail.com>
2026-03-10 17:08:53 +0530

    added API_URL and frontend backend connection

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
 import Banner from "@/components/Banner";
@@ -7,14 +7,28 @@ import NewsLetter from "@/components/NewsLetter";
 import FeaturedProduct from "@/components/FeaturedProduct";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import API_URL from "@/lib/api";
 
 const Home = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await fetch(`${API_URL}/api/products`);
+      const data = await res.json();
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <>
       <Navbar/>
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
-        <HomeProducts />
+        <HomeProducts products={products} />
         <FeaturedProduct />
         <Banner />
         <NewsLetter />
